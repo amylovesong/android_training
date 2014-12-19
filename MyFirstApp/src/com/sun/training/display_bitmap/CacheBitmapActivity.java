0cf3d5c85df712aca4964a4bd1637c7aa2f5ee43
sunxiaoling <amylovesong.sun@gmail.com>
2014-12-19 18:30:22 +0800

    create CacheBitmapActivity

---
+++ MyFirstApp/src/com/sun/training/display_bitmap/CacheBitmapActivity.java
@@ -0,0 +1,12 @@
+package com.sun.training.display_bitmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CacheBitmapActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
