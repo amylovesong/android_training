592587ecd7ad5aa6904268c30db05d07a91d10d8
sunxiaoling <amylovesong.sun@gmail.com>
2015-07-22 23:23:52 +0800

    Zoom-out page transformer

--- MyTrainingCodeNew/app/src/main/java/com/sun/training/animation/ScreenSlidePagerActivity.java
+++ MyTrainingCodeNew/app/src/main/java/com/sun/training/animation/ScreenSlidePagerActivity.java
@@ -1,14 +1,13 @@
 package com.sun.training.animation;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
+import android.view.Window;
 
 import com.sun.training.R;
 
@@ -17,26 +16,31 @@ import com.sun.training.R;
  */
 public class ScreenSlidePagerActivity extends FragmentActivity {
     private static final int NUM_PAGES = 5;
-    private static final String TAG = ScreenSlidePagerActivity.class.getSimpleName();
+    private static final String TAG = "ScreenSlidePagerActivity";
 
     private ViewPager mPager;
 
     private PagerAdapter mPagerAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_screen_slide);
 
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
-        Log.d(TAG, "onCreate");
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+        if(mPager.getCurrentItem()==0){
+            super.onBackPressed();
+        }else {
+            mPager.setCurrentItem(mPager.getCurrentItem()-1);
+        }
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
@@ -46,7 +50,9 @@ public class ScreenSlidePagerActivity extends FragmentActivity {
 
         @Override
         public Fragment getItem(int i) {
-            return new ScreenSlidePageFragment();
+            ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
+            fragment.setPageIndex(i);
+            return fragment;
         }
 
         @Override
