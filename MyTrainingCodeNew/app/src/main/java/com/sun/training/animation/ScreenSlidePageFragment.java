33eade4f05e7a4c5eb8ce2eee0791ebf1cd1b280
amylovesong <amylovesong.sun@gmail.com>
2015-07-22 18:31:22 +0800

    use ViewPager for screen slide animation

---
+++ MyTrainingCodeNew/app/src/main/java/com/sun/training/animation/ScreenSlidePageFragment.java
@@ -0,0 +1,33 @@
+package com.sun.training.animation;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sun.training.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScreenSlidePageFragment extends Fragment {
+
+
+    public ScreenSlidePageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container,false);
+
+        return rootView;
+    }
+
+
+}
---
+++ MyTrainingCodeNew/app/src/main/java/com/sun/training/animation/ScreenSlidePagerActivity.java
@@ -0,0 +1,57 @@
+package com.sun.training.animation;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+import com.sun.training.R;
+
+/**
+ * Created by sunxiaoling on 2015/7/22.
+ */
+public class ScreenSlidePagerActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 5;
+    private static final String TAG = ScreenSlidePagerActivity.class.getSimpleName();
+
+    private ViewPager mPager;
+
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_screen_slide);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
