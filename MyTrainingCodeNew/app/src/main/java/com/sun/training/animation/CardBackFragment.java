65a2d18c2fe87818310696b9a8d2074a6ce840bf
amylovesong <amylovesong.sun@gmail.com>
2015-07-23 17:56:44 +0800

    Card Flip Animations

---
+++ MyTrainingCodeNew/app/src/main/java/com/sun/training/animation/CardBackFragment.java
@@ -0,0 +1,17 @@
+package com.sun.training.animation;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sun.training.R;
+
+public class CardBackFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_card_back, container, false);
+	}
+}
