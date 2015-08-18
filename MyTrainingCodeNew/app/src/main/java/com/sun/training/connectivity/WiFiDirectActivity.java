67ed77a43ef1bf80fe05082ce9122e265aaa822b
sunxiaoling <amylovesong.sun@gmail.com>
2015-08-18 22:49:23 +0800

    Using Wifi P2P for Service Discovery

--- MyTrainingCodeNew/app/src/main/java/com/sun/training/connectivity/WiFiDirectActivity.java
+++ MyTrainingCodeNew/app/src/main/java/com/sun/training/connectivity/WiFiDirectActivity.java
@@ -9,19 +9,25 @@ import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sunxiaoling on 15/8/8.
  */
 public class WiFiDirectActivity extends Activity {
     protected static final String TAG = WiFiDirectActivity.class.getSimpleName();
+    private static final int SERVER_PORT = 0;
+
     private final IntentFilter intentFilter = new IntentFilter();
     private WifiP2pManager mManager;
     private WifiP2pManager.Channel mChannel;
@@ -133,5 +139,86 @@ public class WiFiDirectActivity extends Activity {
         }
     };
 
+    //using wifi p2p for service discovery
+    @SuppressWarnings("NewApi")
+    private void startRegistration() {
+        Map record = new HashMap();
+        record.put("listenport", String.valueOf(SERVER_PORT));
+        record.put("buddyname", "John Doe" + (int) (Math.random() * 1000));
+        record.put("available", "visible");
 
+        WifiP2pDnsSdServiceInfo serviceInfo =
+                WifiP2pDnsSdServiceInfo.newInstance("_test", "_presence._tcp", record);
+
+        mManager.addLocalService(mChannel, serviceInfo, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                //Command successful!
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                //Command failed. Check for P2P_UNSUPPORTED, ERROR, or BUSY
+            }
+        });
+    }
+
+    final HashMap<String, String> buddies = new HashMap<>();
+
+    @SuppressWarnings("NewApi")
+    private void discoverService() {
+        WifiP2pManager.DnsSdTxtRecordListener txtListener = new WifiP2pManager.DnsSdTxtRecordListener() {
+            @Override
+            public void onDnsSdTxtRecordAvailable(
+                    String fullDomainName, Map<String, String> record, WifiP2pDevice device) {
+                Log.d(TAG, "DnsSdTxtRecord available -" + record.toString());
+                buddies.put(device.deviceAddress, record.get("buddyname"));
+            }
+        };
+
+        WifiP2pManager.DnsSdServiceResponseListener servListener = new WifiP2pManager.DnsSdServiceResponseListener() {
+            @Override
+            public void onDnsSdServiceAvailable(String instanceName, String registrationType,
+                                                WifiP2pDevice resourceType) {
+                resourceType.deviceName = buddies.containsKey(resourceType.deviceAddress) ? buddies
+                        .get(resourceType.deviceAddress) : resourceType.deviceName;
+
+                //add to the custom adapter defined specifically for showing wifi devices.
+
+                Log.d(TAG, "onBonjourServiceAvailabe " + instanceName);
+            }
+        };
+
+        mManager.setDnsSdResponseListeners(mChannel, servListener, txtListener);
+
+        WifiP2pDnsSdServiceRequest serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+        mManager.addServiceRequest(mChannel, serviceRequest, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                //success!
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                //command failed, Check for P2P_SUPPORTED, ERROR or BUSY
+            }
+        });
+        mManager.discoverServices(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                if(reason == WifiP2pManager.P2P_UNSUPPORTED){
+                    Log.d(TAG, "P2P isn't supported on this device.");
+                }else if(reason == WifiP2pManager.ERROR){
+
+                }else if (reason == WifiP2pManager.BUSY){
+
+                }
+            }
+        });
+    }
 }
