d4c15e9ffe6eaf4f44e3c92c9c0601415b0cb5df
sunxiaoling <amylovesong.sun@gmail.com>
2016-01-08 18:11:49 +0800

    Sending a Simple Request

--- MyTrainingCodeNew/src/main/java/com/sun/training/connectivity/VolleyActivity.java
+++ MyTrainingCodeNew/src/main/java/com/sun/training/connectivity/VolleyActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.sun.training.R;
 
 public class VolleyActivity extends Activity {
+    public static final String TAG = "MyTag";
+    StringRequest stringRequest;
+    RequestQueue mRequestQueue;
     TextView textView;
 
     @Override
@@ -17,7 +26,38 @@ public class VolleyActivity extends Activity {
         textView = (TextView) findViewById(R.id.text);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        request();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mRequestQueue != null){
+            mRequestQueue.cancelAll(TAG);
+        }
+    }
+
     private void request(){
+        mRequestQueue = Volley.newRequestQueue(this);
+        String url = "http://www.google.com";
+
+        stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText("Response is:" + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        stringRequest.setTag(TAG);
+        mRequestQueue.add(stringRequest);
     }
 
 }
