19f0258fa4d5b54d827e823c3e3fa4a2daf17222
sunxiaoling <amylovesong.sun@gmail.com>
2016-01-11 14:39:45 +0800

    Setting Up a RequestQueue

--- MyTrainingCodeNew/src/main/java/com/sun/training/connectivity/VolleyActivity.java
+++ MyTrainingCodeNew/src/main/java/com/sun/training/connectivity/VolleyActivity.java
@@ -1,13 +1,22 @@
 package com.sun.training.connectivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.LruCache;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.sun.training.R;
@@ -41,7 +50,15 @@ public class VolleyActivity extends Activity {
     }
 
     private void request(){
-        mRequestQueue = Volley.newRequestQueue(this);
+//        mRequestQueue = Volley.newRequestQueue(this);
+
+//        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);//1MB cap
+//        Network network = new BasicNetwork(new HurlStack());
+//        mRequestQueue = new RequestQueue(cache, network);
+//        mRequestQueue.start();
+
+        mRequestQueue = MySingleton.getInstance(getApplicationContext()).getReqeustQueue();
+
         String url = "http://www.google.com";
 
         stringRequest = new StringRequest(Request.Method.GET, url,
@@ -57,7 +74,8 @@ public class VolleyActivity extends Activity {
             }
         });
         stringRequest.setTag(TAG);
-        mRequestQueue.add(stringRequest);
+//        mRequestQueue.add(stringRequest);
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
     }
 
 }
